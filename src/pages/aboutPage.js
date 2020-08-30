ab5eb1452d39dc0f9a3247ce688f66ae6e338cf9
Mrugesh.w <Mrugesh.W@ril.com>
2020-08-31 01:23:05 +0530

    set up routes

---
+++ src/pages/aboutPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AboutPage = () =>(
+    <>
+        <h3>About me and the site</h3>
+    </>
+)
+export default AboutPage;
---
+++ src/pages/storiesPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const StoriesPage = () =>(
+    <>
+        <h3>Travelling stories list</h3>
+    </>
+)
+export default StoriesPage;
